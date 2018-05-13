bc2626dcec448bd90b142970a53f3a8b35e6d36e
David Khourshid <davidkpiano@gmail.com>
2018-05-13 10:08:16 -0400

    Using latest RxJS

--- src/rxio.ts
+++ src/rxio.ts
@@ -1,12 +1,12 @@
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs'
 
-export type TypeMap = Record<string, any>;
+export type TypeMap = Record<string, any>
 
-export type ObservableMap<T extends TypeMap> = { [key in keyof T]: Observable<T[key]> };
+export type ObservableMap<T extends TypeMap> = { [key in keyof T]: Observable<T[key]> }
 export interface Patch<I extends TypeMap, O extends TypeMap> {
-  (inputs: ObservableMap<I>): ObservableMap<O>;
+  (inputs: ObservableMap<I>): ObservableMap<O>
 }
 
 export function Patch<I, O>(project: Patch<I, O>): Patch<I, O> {
-  return project;
+  return project
 }
--- test/rxio.test.ts
+++ test/rxio.test.ts
@@ -1,27 +1,26 @@
-import { Patch } from '../src/rxio';
-import { of } from 'rxjs/Observable/of';
-import { combineLatest } from 'rxjs/Observable/combineLatest';
-import { map } from 'rxjs/operators/map';
-import { marbles } from 'rxjs-marbles';
+import { Patch } from '../src/rxio'
+import { of, combineLatest } from 'rxjs'
+import { map } from 'rxjs/operators'
+import { marbles } from 'rxjs-marbles'
 
 const addPatch = Patch<{ a$: number; b$: number }, { value$: number }>(({ a$, b$ }) => {
   return {
     value$: combineLatest(a$, b$).pipe(map(([a, b]) => a + b))
-  };
-});
+  }
+})
 
 describe('Patch', () => {
   it(
     'should return an Observable map',
     marbles(m => {
-      const values = { a: 2, b: 3, c: 5 };
-      const a$ = m.cold('--a|', values);
-      const b$ = m.cold('---b|', values);
-      const expectedValue$ = m.cold('---c|', values);
+      const values = { a: 2, b: 3, c: 5 }
+      const a$ = m.cold('--(a|)', values)
+      const b$ = m.cold('---(b|)', values)
+      const expectedValue$ = m.cold('---(c|)', values)
 
-      const sum = addPatch({ a$, b$ });
+      const sum = addPatch({ a$, b$ })
 
-      m.expect(sum.value$).toBeObservable(expectedValue$);
+      m.expect(sum.value$).toBeObservable(expectedValue$)
     })
-  );
-});
+  )
+})
